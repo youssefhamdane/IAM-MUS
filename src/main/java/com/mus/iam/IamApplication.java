440d66131d17d74ed318f6d89c98d2350dc202a0
hamdane <youssefamir89@gmail.com>
2022-06-05 14:29:40 +0100

    Initial commit

---
+++ src/main/java/com/mus/iam/IamApplication.java
@@ -0,0 +1,15 @@
+package com.mus.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class IamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamApplication.class, args);
+    }
+
+}
